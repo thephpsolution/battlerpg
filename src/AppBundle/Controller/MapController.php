0b502241d0e0e529cadd0ef26ba5793f99df740c
Jonathan McCartney <jonathaninthefield@gmail.com>
2016-01-02 16:54:37 -0600

    Added other build icons

--- src/AppBundle/Controller/MapController.php
+++ src/AppBundle/Controller/MapController.php
@@ -10,6 +10,7 @@ use AppBundle\ValueObject\MapLayersUpdate;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class MapController extends Controller
 {
@@ -19,8 +20,9 @@ class MapController extends Controller
             'action' => $this->generateUrl('update_map')
         ]);
     }
+
     /**
-     * @Route("/map/view", name="view_map")
+     * @Route("/", name="view_map")
      */
     public function viewAction()
     {
@@ -30,7 +32,38 @@ class MapController extends Controller
             $layers = new MapLayersUpdate($existing);
         }
         $form = $this->getForm($layers);
+
+
+        $request = $this->container->get('request');
+        /* @var $request \Symfony\Component\HttpFoundation\Request */
+        $session = $request->getSession();
+        /* @var $session \Symfony\Component\HttpFoundation\Session\Session */
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
+            $error = $error->getMessage();
+        }
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
+
+        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
+
+        $this->container->get('security.context')->getToken()->getUser()->addRole('ROLE_ADMIN');
         return $this->render('AppBundle:Map:view.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+            'csrf_token' => $csrfToken,
+            'user'      => $user = $this->container->get('security.context')->getToken()->getUser(),
             'form'  => $form->createView(),// ...
             'update' => $layers
         ));
---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\MapLayer;
+use AppBundle\Entity\MapPointSet;
+use AppBundle\Entity\User;
+use AppBundle\Form\MapLayersType;
+use AppBundle\Map\MapEditorBuilder;
+use AppBundle\ValueObject\MapLayersUpdate;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
+
+class SecurityController extends Controller
+{
+    private function getForm($layers = null)
+    {
+        return $this->createForm(MapLayersType::class, $layers ?: (new MapEditorBuilder())->buildForForm(), [
+            'action' => $this->generateUrl('update_map')
+        ]);
+    }
+    /**
+     * @Route("/game-login", name="game_login")
+     */
+    public function loginAction()
+    {
+        /** @var User $user */
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        return $response = new JsonResponse([
+            'user' => (object)[
+                'name' => $user->getUsername(),
+                'admin' => $user->hasRole('ROLE_ADMIN'),
+            ]
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/map/update", name="update_map")
+
+     */
+    public function updateLayersAction(Request $request)
+    {
+        $form = $this->getForm();
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            foreach ($form->getData()->getMapLayers() as $layer) {
+                $this->getDoctrine()->getEntityManager()->persist($layer);
+            }
+            $this->getDoctrine()->getEntityManager()->flush();
+        }
+        return $this->redirectToRoute('view_map');
+    }
+
+}
